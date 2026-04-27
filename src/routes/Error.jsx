d6a5a717151f85be842509ee48d3a678734df303
maria <malubf20@gmail.com>
2026-04-27 11:16:43 -0300

    feat:adicionando a página de error

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,16 +1,16 @@
 const Error = () => {
   return (
-    <section className="min-h-[70vh] flex items-center justify-center bg-[#7AD1C3]/20 px-6 py-12">
-      <div className="w-full max-w-2xl rounded-3xl bg-white shadow-xl border border-[#7AD1C3] p-10 text-center">
-        <div className="mx-auto mb-6 flex h-20 w-20 items-center justify-center rounded-full bg-[#7AD1C3] text-white text-3xl font-bold shadow-md">
+    <section className="min-h-[70vh] flex items-center justify-center bg-[#EBEBDF]/20 px-6 py-12">
+      <div className="w-full max-w-2xl rounded-3xl bg-white shadow-xl border border-[#A8C2E0] p-10 text-center">
+        <div className="mx-auto mb-6 flex h-20 w-20 items-center justify-center rounded-full bg-[#A8C2E0] text-white text-3xl font-bold shadow-md">
           404
         </div>
 
-        <h1 className="text-4xl font-extrabold text-[#1c9770ff] mb-3">
+        <h1 className="text-4xl font-extrabold text-[#83afe1] mb-3">
           Erro 404
         </h1>
 
-        <h2 className="text-2xl font-semibold text-[#93CB52] mb-4">
+        <h2 className="text-2xl font-semibold text-[#A8C2E0]mb-4">
           Página não encontrada
         </h2>
 
@@ -21,7 +21,7 @@ const Error = () => {
         <div className="mt-8">
           <a
             href="/"
-            className="inline-block rounded-full bg-[#1c9770ff] px-6 py-3 font-medium text-white shadow-md transition-all duration-300 hover:bg-[#93CB52] hover:text-[#1c9770ff]"
+            className="inline-block rounded-full bg-[#EA631B] px-6 py-3 font-medium text-white shadow-md transition-all duration-300 hover:bg-[#A8C2E0] hover:text-[#EA631B]"
           >
             Voltar para a Home
           </a>
