8537f3c7f3b03032a66a254dd3ea5f76194eb7f6
“Roberta“ <robertamsantos247@gmail.com>
2026-04-27 16:56:21 -0300

    style: Alterando botão

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { BrowserRouter as Browser, Routes, Route } from "react-router-dom";
+
 function App() {
 
   return (
     <>
-      <Card
-        imagem="/capa.jpg"
-        titulo="Pixel Quest: Lost Kingdom"
-        preco="R$ 79,90"
-        precoOriginal="R$ 159,90"
-        desconto="−50%"
-        review="5,0 · 1.2k"
-        adicionarCarrinho={() => console.log('add')}
-      />
+ 
     </>
   )
 }
