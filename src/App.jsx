d12faf4750a086c5ccb6b391006fa08e4e420888
maria <malubf20@gmail.com>
2026-04-27 10:32:51 -0300

    feat:adicionando a estrutura do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { BrowserRouter as Browser, Routes, Route } from "react-router-dom";
-function App() {
+import Menu from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./routes/Home";
+import Suporte from "./routes/Suporte";
+import Error from "./routes/Error";
+import Produto from "./routes/Produto";
+import "./index.css";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Browser>
+      <div className="min-h-screen flex flex-col">
+        <Header/>
+
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/produto" element={<Produto />} />
+            <Route path="/suporte" element={<Suporte />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Browser>
+  );
 }
 
-export default App
+export default App;
